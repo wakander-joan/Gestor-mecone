94d20eb2517db02a07335bd3c739070a03561b0b
joan <https://github.com/wakander-joan/Teste.git>
2022-08-28 11:11:12 -0300

    Projeto Startado

---
+++ src/test/java/br/com/mecone/gestor/GestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mecone.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
