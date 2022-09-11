6a475ca3f1fb6a5be7f0110fb62d1898ed1cb37e
joan <https://github.com/wakander-joan/Teste.git>
2022-09-11 13:41:58 -0300

    Primeiro commit Busca todos por idEmpresa funcionando

--- src/main/java/br/com/mecone/gestor/produto/api/application/ProdutoAPI.java
+++ src/main/java/br/com/mecone/gestor/produto/api/application/ProdutoAPI.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping ("/v1/produto")
 public interface ProdutoAPI {
