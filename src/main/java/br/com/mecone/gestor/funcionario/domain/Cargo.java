2c1a55bb0b0d9435930d59fac09c2e901d6a2893
joan <https://github.com/wakander-joan/Teste.git>
2022-09-12 22:20:17 -0300

    Domain de Funcionario Implementado

---
+++ src/main/java/br/com/mecone/gestor/funcionario/domain/Cargo.java
@@ -0,0 +1,5 @@
+package br.com.mecone.gestor.funcionario.domain;
+
+public enum Cargo {
+	VENDEDOR, GERENTE, MECANICO, SECRETARIO
+}
---
+++ src/main/java/br/com/mecone/gestor/funcionario/domain/Funcionario.java
@@ -0,0 +1,40 @@
+package br.com.mecone.gestor.funcionario.domain;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Funcionario {
+	private static int geradorCodigo = 0;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idFuncionario;
+	@NotBlank
+	private UUID idEmpresa;
+	@NotBlank
+	private String nome;
+	@NotNull
+	private Cargo cargo;
+	
+	private int codigoFuncionario = geradorCodigo++;
+
+}
